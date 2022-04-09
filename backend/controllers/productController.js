61702bbcb23220aaf949bec50ab93066889c9f02
Kashif <jamshedk@qubitinformationsystems.com>
2022-04-09 21:12:36 +0530

    Update and Add single product added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,9 @@
+const { findByIdAndUpdate } = require('../models/product');
 const Product = require('../models/product')
 
 //create new Product => /api/v1/product/new
 
 exports.newProduct = async(req,res,next)=>{
-    
   const product = await Product.create(req.body);
   res.status(201).json({ 
     success: true,
@@ -14,10 +14,51 @@ exports.newProduct = async(req,res,next)=>{
 //Get All Products => /api/v1/products
 exports.getProducts = async (req,res, next)=>{
   const products = await Product.find();
+ 
   res.status(200).json({
       sucess: true,
       count: products.length,
       products
   })
+}
+
+// Update Product => /api/v1/product/id
+exports.updateProduct = async(req,res,next) =>{
+
+  let product = await Product.findById(req.params.id);
+  console.log(product,"hiii");
+  if(!product){
+    return res.status(404).json({
+      sucess: false,
+      message: 'Product not found'
+    })
+  }
+   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+     new: true,
+     runvalidators: true,
+     useFindAndModify: false
+   })
+   res.status(200).json({
+    sucess: true,
+    product
+  })
+
+}
+
+// Get Single product => /api/v1/product/:id
+
+exports.getsingleProduct = async(req,res,next) =>{
+
+  const product = await Product.findById(req.params.id)
+  if(!product){
+    return res.status(404).json({
+      sucess: false,
+      message: 'Product not found'
+    })
+  }
+    res.status(200).json({
+      sucess: true,
+      product
+    })
 }
 
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router();
 
-const { getProducts, newProduct } = require('../controllers/productController')
+const { getProducts, newProduct, getsingleProduct, updateProduct } = require('../controllers/productController')
 
 
 
  router.route('/products').get(getProducts);
+ router.route('/products/:id').get(getsingleProduct);
+
  router.route('/products/new').post(newProduct);
+ router.route('/admin/products/:id').post(updateProduct);
+ 
  module.exports = router;
